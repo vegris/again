1be177f345332074dd07b9a927db3c971dfc8110
vegris <1cheaterok1@gmail.com>
2025-01-21 18:01:14 +0300

    Add Dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Again.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -21,7 +22,16 @@ defmodule Again.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_local_path: "priv/plts",
+      flags: ~w[error_handling extra_return missing_return underspecs unmatched_returns]a
     ]
   end
 end
