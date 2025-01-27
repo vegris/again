572b37c65cab7b854829cff219542e9a5cb33900
vegris <1cheaterok1@gmail.com>
2025-01-27 13:32:21 +0300

    Relax Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Again.MixProject do
     [
       app: :again,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
